29927853fdc144628c418e7a199e8bf6a1b2b8fa
Mertkan Uzunparmak <mertkan@mobilike.com>
2016-05-04 12:33:37 +0300

    Initial commit

---
+++ app/src/main/java/com/zcreen/adapter/SurveysAdapter.java
@@ -0,0 +1,62 @@
+package com.zcreen.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.zcreen.R;
+import com.zcreen.core.BaseRecyclerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by mertkanuzunparmak on 10/04/16.
+ * mertkan@mobilike.com
+ */
+public class SurveysAdapter extends BaseRecyclerAdapter<SurveysAdapter.ViewHolder> {
+
+    private List<String> comments;
+    private Context context;
+
+    public SurveysAdapter(Context context, List<String> comments) {
+        this.context = context;
+        this.comments = comments;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayout = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_comments, parent, false);
+        return new ViewHolder(itemLayout);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String comment = comments.get(position);
+        holder.comment.setText(comment);
+        Picasso.with(context).load(R.mipmap.ic_launcher).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return comments.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView comment;
+        public ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            comment = (TextView) itemView.findViewById(R.id.list_item_comments_comment_textView);
+            imageView = (ImageView) itemView.findViewById(R.id.list_item_user_display_photo);
+
+        }
+    }
+}
